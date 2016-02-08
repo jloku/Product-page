4219efb954f1f2df1a3f35070dec581040724eb8
Jlorky <jloku.77@gmail.com>
2016-02-08 21:21:10 +0200

    Second commit

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,15 @@ function Main(){
         console.log(data);
 		$.each(data.Products, function( index, value ) {
 			var elements = [];
+			var firstindex = 0;
 			$.each(value.rates, function( in_index, in_value ) {
+				if (firstindex==0){firstindex = in_index}
 				var element = '<option>'+in_index+'</option>'
 				elements.push(element);
 			});
 			$('<div/>', {
-			id: index,
-			title: 'Become a Googler',
-			rel: 'external',
-		}).addClass("product_wrap").appendTo('.Products').append("<div class='product'><img class='logo' src='" +value.image+"'><select size='1' class='rates' name = " + index + ">"+ elements +"</select></div>");
+			id: index
+		}).addClass("product_wrap").appendTo('.Products').append("<div class='product'><img class='logo' src='" +value.image+"'><select size='1' class='rates' name = '" + index + "'>"+ elements +"</select><h2 class='price'>Price:</h2><h2 class='price price_dinamic'>"+value.rates[firstindex]+"</h2></div>");
 		
 		});
 
@@ -23,37 +23,52 @@ function Main(){
 	$('body').on('click', '.product_wrap', function(){
 		  if ( state == -1 ) {
 				$(this).animate({
-					  width: "42%",
-					  height:"220px"
-				}, 500 );
-				$(this).find('.rates').delay(500).animate({
+					  width: "49%",
+					  height:"220px",
+				}, 200 );
+				$(this).find('.rates').delay(200).animate({
 					  opacity: 1
-				}, 300 );
-				
+				}, 200 );
+				$(this).find('.price').delay(200).animate({
+					  opacity: 1
+				}, 200 );
 				state = $('.product_wrap').index(this);
 		  } else {
 				if (!($('.product_wrap').index(this) ==state)){
-					$($('.product_wrap').get(state)).delay(300).animate({
+					$($('.product_wrap').get(state)).delay(200).animate({
 						  width: "24%",
 						  height:"100px",
-					}, 500 );
+						  background:"#ccf2db"
+					}, 200 );
 					$($('.product_wrap').get(state)).find('.rates').animate({
 					  opacity: 0
-					}, 300 );
+					}, 200 );
+					$($('.product_wrap').get(state)).find('.price').animate({
+					  opacity: 0
+					}, 200 );
 					$(this).animate({
-						  width: "42%",
+						  width: "49%",
 						  height:"220px"
-					}, 500 );
-					$(this).find('.rates').delay(500).animate({
+					}, 200 );
+					$(this).find('.rates').delay(200).animate({
 					  opacity: 1
-				}, 300 );
+					}, 200 );
+					$(this).find('.price').delay(200).animate({
+					  opacity: 1
+					}, 200 );
 				state = $('.product_wrap').index(this);
 					state = $('.product_wrap').index(this);
 				}
 		  }
-		});
-	
-
+	});
+	var Prod;
+	$.getJSON("data.json", function(json) {
+		Prod = json;
+	});
+		
+	$('body').on('change', '.rates', function(){
+		$(this).parent().find('.price_dinamic').text(Prod.Products[$(this).attr('name')].rates[$(this).val()])
+	});
 
 }
 $( Main )
